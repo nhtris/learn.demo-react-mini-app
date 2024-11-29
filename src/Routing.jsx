4e862ad0b0b9feb099f23ea33a59103d3ce39504
nhtri <huutri26697@gmail.com>
2024-11-29 13:52:50 +0700

    naviagete

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SlideRoutes from "react-slide-routes";
 import HomePage from "./page/home";
 import ProductsPage from "./page/products";
 import ProductDetail from "./page/product-detail";
 import AppInfo from "./page/app-info";
-import SlideRoutes from "react-slide-routes";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function Routing() {
   return (
     <Router>
-      {/* //   <Routes> */}
+      <ScrollToTop />
       <SlideRoutes>
         <Route path="*" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/product-detail" element={<ProductDetail />} />
         <Route path="/app-info" element={<AppInfo />} />
-        {/* </Routes> */}
       </SlideRoutes>
     </Router>
   );
